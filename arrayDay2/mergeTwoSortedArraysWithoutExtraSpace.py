31077fdd7f9766751666c33aadaeaa73df290449
vikas <vikasvimal1999@gmail.com>
2022-07-19 22:01:11 +0530

    feat : solution3 in python

---
+++ arrayDay2/mergeTwoSortedArraysWithoutExtraSpace.py
@@ -0,0 +1,41 @@
+# Problem statement: Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. Merge them in sorted order. Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.
+# Examples:
+# Example 1:
+# Input: 
+# n = 4, arr1[] = [1 4 8 10] 
+# m = 5, arr2[] = [2 3 9]
+# Output: 
+# arr1[] = [1 2 3 4]
+# arr2[] = [8 9 10]
+# Explanation:
+# After merging the two non-decreasing arrays, we get, 1,2,3,4,8,9,10.
+# Example2:
+# Input: 
+# n = 4, arr1[] = [1 3 5 7] 
+# m = 5, arr2[] = [0 2 6 8 9]
+# Output: 
+# arr1[] = [0 1 2 3]
+# arr2[] = [5 6 7 8 9]
+# Explanation:
+# After merging the two non-decreasing arrays, we get, 0 1 2 3 5 6 7 8 9.
+
+# Time complexity: O(logn)
+# Space Complexity: O(1)
+
+def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    last = m + n -1
+    
+    while m > 0 and n > 0:
+        if nums1[m-1] > nums2[n-1]:
+            nums1[last] = nums1[m-1]
+            m -= 1
+        else:
+            nums1[last] = nums2[n-1]
+            n -= 1
+        
+        last -= 1
+
+    while n > 0:
+        nums1[last] = nums2[n-1]
+        n = n-1 
+        last = last -1
